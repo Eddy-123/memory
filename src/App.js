26cf966755551d8dc9448ce1253a50d714e5ffd2
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-08-02 12:13:43 +0100

    define propTypes for the components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header gameState="in progress" />
+        <Header />
         <Body />
         <Footer />
       </div>
