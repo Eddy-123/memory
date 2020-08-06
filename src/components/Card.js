01e742c2eabaae098b663cc4459d25bf7048b0c3
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-08-06 23:42:35 +0100

    navbar with bootstrap

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Card = ({ card, feedback }) => (
-  <div className={`card ${feedback}`}>
+const HIDDEN_SYMBOL = "❓";
+
+const Card = ({ card, feedback, onClick }) => (
+  <div className={`card ${feedback}`} onClick={() => onClick(card)}>
     <span className="symbol">
       {feedback === "hidden" ? HIDDEN_SYMBOL : card}
     </span>
   </div>
 );
+
+Card.propTypes = {
+  card: PropTypes.string.isRequired,
+  feedback: PropTypes.string.isRequired
+};
+
+Card.defaultProps = {
+  card: HIDDEN_SYMBOL,
+  feedback: "hidden"
+};
 
 export default Card;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Header({ gameState }) {
   return (
     <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Memory
+        </Link>
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <h1>Memory, the game is {gameState}</h1>
     </header>
   );
 }
+
+Header.propTypes = {
+  gameState: PropTypes.string.isRequired
+};
+Header.defaultProps = {
+  gameState: "in progress"
+};
