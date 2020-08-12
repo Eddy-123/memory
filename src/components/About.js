5bf4d9b4da1ce0bafe409966feac9fa0b9c498f9
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-08-12 15:19:57 +0100

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function About() {
-  return <h1>About</h1>;
+export default class About extends Component {
+  state = {
+    city: "",
+    weather: ""
+  };
+  //Arrow fx for binding
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { city } = this.state;
+    let apiRequest = new XMLHttpRequest();
+    apiRequest.onreadystatechange = () => {
+      if (apiRequest.status === 404) {
+        this.setState({
+          weather: "Sorry, '" + city + "' is not recognized by the service"
+        });
+      } else if (apiRequest.readyState === 4) {
+        const response = JSON.parse(apiRequest.response);
+        this.setState({
+          weather:
+            "The weather in " +
+            response.name +
+            " is " +
+            response.weather[0].description +
+            "."
+        });
+      }
+    };
+    apiRequest.open(
+      "GET",
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e"
+    );
+    apiRequest.send();
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ city: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <h2>The weather</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Enter your city"
+            value={this.state.city}
+            onChange={this.handleInputChange}
+            type="text"
+          />
+          <button type="submit">WHEATHER</button>
+        </form>
+        {this.state.weather !== "" && <p>{this.state.weather}</p>}
+      </div>
+    );
+  }
 }
